410966442b1d4e6793a601347e91e43d8b8909b7
onishechko <onishechko.lviv@gmail.com>
2021-09-27 12:18:06 +0300

    Task 2

---
+++ Task2/Task2/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+   public class Item <T>
+    {
+        public T Data { get; set; }
+
+        public Item<T> Next { get; set; }
+
+        public Item(T data)
+        {
+            if (data == null)
+                throw new ArgumentNullException(nameof(data));
+            Data = data;
+        }
+    }
+}
