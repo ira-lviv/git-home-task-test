17991d26ef2d7aa70f4305cbfb3ec61ff78eb9ef
onishechko <onishechko.lviv@gmail.com>
2021-09-29 11:45:13 +0300

    Task3

---
+++ Task3/Task3/GetStrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public delegate void Input(string text);
+    
+    
+    class GetStrings
+    {
+        // public  Input input;
+        //public event Input input;
+        public Action<string> input;
+        public void Run() {
+            var text = "";
+            do
+            {
+                text = Console.ReadLine();
+                input(text);
+
+            } while (text!="0");
+        }
+    }
+}
