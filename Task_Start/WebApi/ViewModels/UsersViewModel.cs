2faddd8c37827a7ba5f0c5fb9f2caaab716904d1
onishechko <onishechko.lviv@gmail.com>
2021-11-23 10:06:41 +0200

    Web_api

---
+++ Task_Start/WebApi/ViewModels/UsersViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WebApi.ViewModels
+{
+    public class UsersViewModel
+    {
+        public List<string> UserNames{ get; set; } = new List<string>();
+    }
+}
